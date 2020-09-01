5697672d535a7d280bc3add1b270c4c5f447891d
Simon <bardrabbit@gmail.com>
2020-09-01 19:50:55 -0400

    build skeleton of the exercise

---
+++ src/BoxList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BoxList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>boxlist</div>;
+  }
+}
+
+export default BoxList;
